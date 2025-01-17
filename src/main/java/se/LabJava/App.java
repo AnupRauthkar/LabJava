3ce4bbb75da4e831401beac222da7ed006fe08fc
Anup Rauthkar <anuprauthkar@gmail.com>
2025-01-18 04:51:56 +0530

    Lab Java

---
+++ src/main/java/se/LabJava/App.java
@@ -0,0 +1,13 @@
+package se.LabJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
